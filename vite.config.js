e11e4efe1afc7566b99b974cc20bd70662d3d1da
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-02-12 17:25:38 +0600

    more fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: "/Marknow-portfolio-task/",
+  base: "/Marknow-portfolio-task",
 })
