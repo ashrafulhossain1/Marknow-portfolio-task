02869d9cc28130213827e1958b42f8a1134d5d7a
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-02-12 16:20:17 +0600

    Fixed GitHub Pages routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import MainLayout from './MainLayout/MainLayout';
-import Home from './Pages/Home/Home';
+import MainLayout from "./MainLayout/MainLayout";
+import Home from "./Pages/Home/Home";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainLayout></MainLayout>,
-    children: [
-      {
-        path: '/',
-        element: <Home></Home>
-      }
-    ]
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+      ],
+    },
+  ],
+  { basename: "/Marknow-portfolio-task/" } // ✅ এখানে basename যোগ করো
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
